6bf0bf2b988a9669eafbcb1468dc4494c432b24e
Jason Kim <jason@pushbyte.io>
2015-04-16 00:22:36 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/* Cells - An evolutionary game
+ * Jason Kim
+ * 4/15/2015
+ */
+
+'use strict';
+var ticker = require('ticker');
+
+$(document).ready(function(){
+  var canvas = document.getElementById('field'),
+      ctx    = canvas.getContext('2d');
+});
