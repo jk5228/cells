f1f80f24267968c384efbd330e2fe60d32f452c5
Jason Kim <jasonkim.0815@gmail.com>
2015-05-11 17:23:40 -0400

    boid speed is now any real value from 1 to maxspeed

--- app.js
+++ app.js
@@ -11,21 +11,22 @@ var canvas = document.getElementById('canvas');
 var maxwidth = canvas.width;
 var maxheight = canvas.height;
 var maxspeed = 5;
+var radius = 20;
+var paused = false;
 
 // Cursor
-var pt = new Point(Math.floor(Math.random() * maxwidth),
-                   Math.floor(Math.random() * maxheight));
-
 var cursor = {
-  point: pt,
+  point: view.center,
   path: new Path.Circle({
-    center: pt,
-    radius: 20,
+    center: view.center,
+    radius: radius,
     fillColor: 'white'
   })
 };
 
 // Boid class
+var colors = [new Color('blue'), new Color('red'), new Color('green')];
+
 function Boid(position,angle,speed,color) {
   var position = position || new Point(0,0);
   this.angle = angle || 90;
@@ -37,7 +38,7 @@ function Boid(position,angle,speed,color) {
       [position.x+10,position.y-10],
       [position.x-10,position.y-10]
     ],
-    fillColor: this.color || new Color('red'),
+    fillColor: new Color('red'),
     closed: true
   });
   this.path.rotate(this.angle-90,this.path.position);
@@ -50,32 +51,33 @@ Boid.prototype.iterate = function() {
   //   // view.pause();
   //   return;
   // }
+  if (!paused) {
+    // Update position
+    var rad = this.angle * Math.PI/180;
+    var dX = Math.cos(rad) * this.speed;
+    var dY = Math.sin(rad) * this.speed;
+    this.path.translate(new Point(dX,dY));
 
-  // Update position
-  var rad = this.angle * Math.PI/180;
-  var dX = Math.cos(rad) * this.speed;
-  var dY = Math.sin(rad) * this.speed;
-  this.path.translate(new Point(dX,dY));
-
-  // Update angle
-  var vector = cursor.point - this.path.position;
-  var rawdiff = Math.floor(vector.angle) - this.angle;
-  // console.log(rawdiff);
-  if ((rawdiff < 0 && rawdiff > -180) || rawdiff >= 180) {
-    this.angle -= this.speed;
-    if (this.angle < -180)
-      this.angle += 360;
-    this.path.rotate(-this.speed,this.path.position);
-  }
-  else if ((rawdiff > 0 && rawdiff < 180) || rawdiff <= -180) {
-    this.angle += this.speed;
-    if (this.angle > 180)
-      this.angle -= 360;
-    this.path.rotate(this.speed,this.path.position);
+    // Update angle
+    var vector = cursor.point - this.path.position;
+    var rawdiff = Math.floor(vector.angle) - this.angle;
+    // console.log(rawdiff);
+    if ((rawdiff < 0 && rawdiff > -180) || rawdiff >= 180) {
+      this.angle -= this.speed;
+      if (this.angle < -180)
+        this.angle += 360;
+      this.path.rotate(-this.speed,this.path.position);
+    }
+    else if ((rawdiff > 0 && rawdiff < 180) || rawdiff <= -180) {
+      this.angle += this.speed;
+      if (this.angle > 180)
+        this.angle -= 360;
+      this.path.rotate(this.speed,this.path.position);
+    }
   }
 
   // Update color
-  var dist = 30/cursor.path.position.getDistance(this.path.position);
+  var dist = Math.sqrt(cursor.path.area)/cursor.path.position.getDistance(this.path.position);
   this.path.fillColor.brightness = dist;
 
 };
@@ -85,7 +87,7 @@ var boids = [];
 for (var i = 0; i < population; i++) {
   var position = new Point(Math.random() * maxwidth,Math.random() * maxheight);
   var angle = Math.floor(Math.random() * 360) - 180;
-  var speed = Math.ceil(Math.random() * maxspeed);
+  var speed = Math.random() * (maxspeed - 1) + 1;
   var boid = new Boid(position,angle,speed);
   boids.push(boid);
 }
@@ -101,4 +103,8 @@ function onMouseMove(e) {
   cursor.point = e.point;
   cursor.path.translate(delta);
   // view.play();
+}
+
+function onMouseDown(e) {
+  paused = !paused;
 }
